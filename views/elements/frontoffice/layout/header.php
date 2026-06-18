3354c42a0ed4de1b005312de77e351cc522f5ec7
JulieCornuPro <190005554+JulieCornuPro@users.noreply.github.com>
2026-06-18 12:59:36 +0200

    Add files via upload

---
+++ views/elements/frontoffice/layout/header.php
@@ -0,0 +1,7 @@
+<header id="header" class="container clearfix">
+	<a href="index-2.html" id="logo">
+		<img src="<?php echo Router::webroot("img/logo.png");?>" alt="SmartStart">
+	</a>
+	<?php $this->element(FRONTOFFICE.DS.'layout'.DS.'menu.php');?>
+	
+</header>
