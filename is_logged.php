a8b5aca87162694d8dc8b9295622bb1835e072ec
JulieCornuPro <190005554+JulieCornuPro@users.noreply.github.com>
2026-06-18 12:30:24 +0200

    Delete is_logged.php

--- is_logged.php
+++
@@ -1,8 +0,0 @@
-<?php
-// pr($this->request->prefix);
-//redirection vers le formulaire de connexion si l'on essaye d'acceder aux pages de connexion sans etre connecté
-if( isset($this->request->prefix) && $this->request->prefix == 'backoffice' && !Session::check('Backoffice')){
-	Session::setFlash('Merci de bien vouloir vous connecter','info');
-	$this->redirect('connexion');
-}
-?>
