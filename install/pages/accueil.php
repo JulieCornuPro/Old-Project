9ae452f9328353fd81f60384e6a008f15a0a1d5c
JulieCornuPro <190005554+JulieCornuPro@users.noreply.github.com>
2026-06-18 12:39:06 +0200

    Create accueil.php

---
+++ install/pages/accueil.php
@@ -0,0 +1,10 @@
+<?php 
+$httpHost = $_SERVER["HTTP_HOST"];
+if($httpHost == 'localhost' || $httpHost == '127.0.0.1') { $section = 'localhost'; } else { $section = 'online'; }
+?>
+<form action="index.php?step=connect" method="post">
+	<input type="hidden" name="section" value="<?php echo $section; ?>" />
+	<button class="btn btn-large btn-primary" type="submit">Configurer la base de données</button></br>
+	<hr>
+	<?php echo is_active('mod_rewrite');?>
+</form>
